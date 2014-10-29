aef40134f42dc9d34ec51f5931bb9241694042f7
Brendan Erwin <brendanjerwin@gmail.com>
2014-10-29 12:27:11 -0400

    Allows a query string parameter to set Accept
    The _accept query string parameter will set the Accept header as well.

--- mimic.js
+++ mimic.js
@@ -9,12 +9,16 @@ module.exports = function() {
   var defaultType = Mime.default_type;
 
   return function(req, res, next) {
-    var ext = extname(req.path); // NB: req._parsedUrl is created
-    var accept = Mime.lookup(ext);
-    if (accept != defaultType) {
-      req.headers.accept = accept + ';q=1, ' + (req.headers.accept || '');
-      // NB: req.path uses req._parsedUrl.pathname
-      req._parsedUrl.pathname = req.path.slice(0, -ext.length);
+    var acceptParameter = req.param('_accept');
+    if (acceptParameter) {
+      req.headers.accept = acceptParameter;
+    } else {
+        var ext = extname(req.path);
+        var accept = Mime.lookup(ext);
+        if (accept != defaultType) {
+          req.headers.accept = accept + ';q=1, ' + (req.headers.accept || '');
+          req._parsedUrl.pathname = req.path.slice(0, -ext.length);
+        }
     }
     next();
   };
